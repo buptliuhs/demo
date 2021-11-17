6f827e4c2c4d9370ee286c648cdb3ec33000cf2a
Tony Liu <tony.liu@rakenapp.com>
2021-11-17 18:38:49 +1300

    rateLimit

--- src/demo1.ts
+++ src/demo1.ts
@@ -1,3 +1,42 @@
-export function add(a: number, b: number): number {
-  return a + b;
+/**
+ * per customer bucket
+ * * per timeRange bucket (depends on # of seconds)
+ */
+
+const EMPTY_INTERVAL_SECONDS = 3;  // 1 minutes
+const MAX_NUMBER_OF_REQUESTS = 10; // 1 million requests
+const CARRY_OVER_MAX_NUMBER_OF_REQUESTS = 2 * MAX_NUMBER_OF_REQUESTS;
+
+let carryOverBuckets: { [key: number]: number } = {}
+
+/**
+ * Customer can make # requests every # seconds
+ * @param customerId
+ * @return true if the request is allowed, otherwise false
+ */
+export function rateLimit(customerId: number): boolean {
+    if (carryOverBuckets[customerId] === undefined) {
+        carryOverBuckets[customerId] = CARRY_OVER_MAX_NUMBER_OF_REQUESTS;
+    }
+    if (carryOverBuckets[customerId] > 0) {
+        // allow when there are carry over
+        carryOverBuckets[customerId]--;
+        return true;
+    }
+    return false;
 }
+
+/**
+ * Empty bucket
+ */
+function emptyBucket(): void {
+    for (const customerId of Object.keys(carryOverBuckets)) {
+        const id = Number.parseInt(customerId);
+        const sum = carryOverBuckets[id] + MAX_NUMBER_OF_REQUESTS
+        carryOverBuckets[id] = sum > CARRY_OVER_MAX_NUMBER_OF_REQUESTS ? CARRY_OVER_MAX_NUMBER_OF_REQUESTS : sum;
+    }
+}
+
+// Empty bucket per # of minutes
+setInterval(emptyBucket, EMPTY_INTERVAL_SECONDS * 1_000);
+
--- test/demo1.test.ts
+++ test/demo1.test.ts
@@ -1,10 +1,72 @@
-import { add } from "../src/demo1";
+import {rateLimit} from "../src/demo1";
 import * as faker from "faker";
 
-describe("demo1", () => {
-  it("add", () => {
-    const n1 = faker.datatype.number();
-    const n2 = faker.datatype.number();
-    expect(add(n1, n2)).toEqual(n1 + n2);
-  });
+jest.setTimeout(60_000);
+
+const sleep = (milliseconds: number) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+describe("rateLimit", () => {
+    it("rateLimit", () => {
+        const customerId = faker.datatype.number();
+        let allowed: boolean;
+        for (let i = 0; i < 20; i++) {
+            allowed = rateLimit(customerId);
+            expect(allowed).toBe(true);
+        }
+        allowed = rateLimit(customerId);
+        expect(allowed).toBe(false);
+    });
+
+    // it("rateLimit allowed after one interval", async () => {
+    //     const customerId = faker.datatype.number();
+    //     let allowed: boolean;
+    //     for (let i = 0; i < 10; i++) {
+    //         allowed = rateLimit(customerId);
+    //         expect(allowed).toBe(true);
+    //     }
+    //     allowed = rateLimit(customerId);
+    //     expect(allowed).toBe(false);
+    //
+    //     // Wait for 1 minute
+    //     console.log("waiting for 3 seconds");
+    //     await sleep(3 * 1_000);
+    //     console.log("now, make another request");
+    //
+    //     allowed = rateLimit(customerId);
+    //     expect(allowed).toBe(true);
+    // });
+
+    it("rateLimit allowed after one interval (accumulated)", async () => {
+        const customerId = faker.datatype.number();
+        let allowed: boolean;
+        console.log("making 20 requests");
+        for (let i = 0; i < 20; i++) {
+            allowed = rateLimit(customerId);
+            expect(allowed).toBe(true);
+        }
+        allowed = rateLimit(customerId);
+        expect(allowed).toBe(false);
+        // Wait for 1 minute
+        console.log("waiting for 3 seconds");
+        await sleep(3 * 1_000);
+        console.log("now, make round of requests");
+        console.log("making 5 requests");
+        for (let i = 0; i < 5; i++) {
+            allowed = rateLimit(customerId);
+            expect(allowed).toBe(true);
+        }
+        console.log("waiting for 3 seconds");
+        await sleep(3 * 1_000);
+        console.log("now, make round of requests");
+        console.log("making 15 requests");
+        for (let i = 0; i < 15; i++) {
+            allowed = rateLimit(customerId);
+            expect(allowed).toBe(true);
+        }
+        console.log("making another request");
+        allowed = rateLimit(customerId);
+        expect(allowed).toBe(false);
+    });
 });
